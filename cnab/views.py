9739b2e36f9c8841b5e534bfdbbc58a52a18c751
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-02-12 22:29:04 -0300

    feat: add by form

--- cnab/views.py
+++ cnab/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView, Response, status
 from rest_framework.generics import ListAPIView
 from .parsers import FileUploadParser
+from rest_framework.parsers import MultiPartParser, FormParser
 import ipdb
 from .utils import get_type, get_nature, get_date, get_value, get_hour
 from .serializers import CnabSerializer
@@ -12,17 +13,21 @@ from rest_framework.renderers import TemplateHTMLRenderer
 
 # Create your views here.
 class CreateCnabView(ListAPIView, APIView):
-    parser_classes = [FileUploadParser]
+    parser_classes = [FormParser, MultiPartParser]
     queryset = Cnab.objects.all()
     serializer_class = CnabSerializer
     
-    def post(self, request, filename="cnab", format=None):
-        uploaded_file = request.FILES['file']
-        str_text = ''
-        for line in uploaded_file:
-            str_text = str_text + line.decode()
+    def post(self, request, filename=None, format=None):
+        uploaded_file = str(request.data['data'].read())
 
-        cnabs = str_text.split("\r\n")[1:]
+        lines = uploaded_file.split("\\r\\n")
+        
+        cnabs = []
+        for line in lines:
+            if line != "b'":
+                cnabs.append(line)
+
+        cnabs_created = []
 
         for cnab in cnabs:
             cnab_dict = {
@@ -39,7 +44,7 @@ class CreateCnabView(ListAPIView, APIView):
 
             store_obj, created = Store.objects.get_or_create(name=cnab_dict['store_name'], owner_name = cnab_dict['store_owner_name'])
 
-            cnab_already_exists = Cnab.objects.filter(store=store_obj, type=cnab_dict["type"], hour=cnab_dict["hour"], date=cnab_dict["date"]).exists()
+            cnab_already_exists = False  ## Cnab.objects.filter(store=store_obj, type=cnab_dict["type"], hour=cnab_dict["hour"], date=cnab_dict["date"]).exists()
 
             serializer = CnabSerializer(data=cnab_dict)
 
@@ -55,4 +60,7 @@ class CreateCnabView(ListAPIView, APIView):
 
             serializer.save(store=store_obj)
 
-        return Response(status=201)
+            cnabs_created.append(serializer.data)
+
+
+        return Response(data=cnabs_created, status=201)
